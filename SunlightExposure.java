c6b5611e50e0208b4f358dd4d98ac3a857003edf
Dancan Angwenyi <dancangwe@gmail.com>
2018-09-15 23:37:59 +0200

    Update SunlightExposure.java

--- SunlightExposure.java
+++ SunlightExposure.java
@@ -8,20 +8,10 @@ public class SunlightExposure{
    private static ArrayList<Float>TREE_SUNLIGHT = new ArrayList<>();
    private static int NUMBER_OF_TREES;
    private static ArrayList<ArrayList<Float>> sunlight = new ArrayList<>();
-   /**
-   <terrain x size – INT> <terrain y size – INT>
-   <avg. hours sunlight at (0,0) – FLOAT> <avg. hours sunlight at (0, 1) – FLOAT> … <avg.
-   hours sunlight at (xsize-1, ysize-1) – FLOAT>
-   <number of trees – INT>
-   <x corner of tree 0 – INT> <y corner of tree 0 – INT> <canopy extent of tree 0 – INT>
-   …
-   <x corner of tree (numtrees-1) – INT> <y corner of tree (numtrees-1) – INT> <canopy extent
-   of tree (numtrees-1) – INT>
-   **/
+   
    public static void main(String [] args){
    
       Scanner input = new Scanner(System.in);
-      //System.out.println("Enter the Size of the Terrain");
       while(input.hasNextLine())
       {
       
@@ -29,27 +19,27 @@ public class SunlightExposure{
          TERRAIN_WIDTH_X = Integer.parseInt(TERRAIN_VALUES[0]);
          TERRAIN_LENGTH_Y = Integer.parseInt(TERRAIN_VALUES[1]);
          int SUNLIGHT_HOURS_LENGTH = TERRAIN_WIDTH_X * TERRAIN_LENGTH_Y ;
-         //System.out.println("Enter the sunlight Values");
-         
          String SUNLIGHT_HOURS[] = (input.nextLine().split(" "));
+         
          if(SUNLIGHT_HOURS.length != SUNLIGHT_HOURS_LENGTH )
          {
             System.out.println("Some values are missing");
             System.exit(0);
          }
+         
          int d = 0;
-         for (int j = 0; j<TERRAIN_LENGTH_Y ;j++){
-             for (int k = 0; k<TERRAIN_WIDTH_X ;k++){
+         
+         for (int j = 0; j<TERRAIN_LENGTH_Y ;j++)
+         {
+             for (int k = 0; k<TERRAIN_WIDTH_X ;k++)
+             {
                    float value = Float.parseFloat(SUNLIGHT_HOURS[d]);
                    sunlight.add(new ArrayList<>());
                    sunlight.get(j).add(value);
-                   d+=1;
-                   //System.out.print(value+" ");
-              }
-               //System.out.println(); 
-         }
-      // System.out.println("Enter the number of trees");
-       NUMBER_OF_TREES = Integer.parseInt(input.nextLine());
+                   d+=1;    
+              } 
+        }
+        NUMBER_OF_TREES = Integer.parseInt(input.nextLine());
             for(int n =0; n <NUMBER_OF_TREES ; n++){
             float sum = 0;
             String arr[] = input.nextLine().split(" ");
